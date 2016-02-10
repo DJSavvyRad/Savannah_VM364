7dbab936512b812032ddf783d27c6f0196e41c82
Savannah Williams-Radecic <s_williamsradecic@emerson.edu>
2016-02-10 17:43:59 -0500

    Lesson 3

---
+++ Lesson_3/Scriptin1/Assets/_Scripts/Point and Shoot.js
@@ -0,0 +1,15 @@
+﻿ #pragma strict
+ var prefab : GameObject;
+ var distance = 10.0;
+ 
+ function Update () {
+     if (Input.GetMouseButtonDown(0)) {
+     
+         var position = Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+         position = Camera.main.ScreenToWorldPoint(position);
+         var go = Instantiate(prefab, Vector3(0,1,-10), Quaternion.identity) as GameObject;
+         go.transform.LookAt(position);    
+         Debug.Log(position);    
+         go.GetComponent(Rigidbody).AddForce(go.transform.forward * 1000);
+     }
+ }
---
+++ Lesson_3/Scriptin1/Assets/_Scripts/movement.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var movement = true;
+
+function Start () {
+
+}
+function Raise () { 
+	transform.Translate(Vector3.up * Time.deltaTime, Space.World); 
+    if(transform.position.y >= 6){
+    movement = false; 
+    }
+}
+
+function Lower () {
+	transform.Translate(Vector3.down * Time.deltaTime, Space.World); 
+	if(transform.position.y <= -4){
+    movement = true; 
+   }
+
+}
+//var move = true;  
+
+function Update () {
+		// Move the object upward in world space 1 unit/second.
+//		if (transform.position.y < 6) {
+//		transform.Translate(Vector3.up * Time.deltaTime, Space.World); 
+
+	if (movement == true){
+	Raise();
+	}
+	else{
+	Lower();
+	}
+}

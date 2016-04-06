540f4e2369db3b8fadb8f9dafcf6cb0ae89e23b2
Savannah Williams-Radecic <s_williamsradecic@emerson.edu>
2016-04-06 17:50:08 -0400

    More monster coding

--- VM364_Canis/Assets/_Scripts/Creature_Script.js
+++ VM364_Canis/Assets/_Scripts/Creature_Script.js
@@ -1,44 +1,58 @@
+#pragma strict
+
 var animator : Animator;
-
- function OnTriggerEnter(){
- animator.enabled = false;
- }
- function OnTriggerStay(){
-	ChasePlayer();
- }
- function OnTriggerExit(){
- animator.enabled = true;
- }
-//Following script from http://answers.unity3d.com/questions/760751/monster-patrolling.html
-
 var Player : Transform;
- var MoveSpeed = 4;
- var MaxDist = 10;
- var MinDist = 5;
- 
- 
- 
- 
-// function Start () 
-// {
-// 
-// }
-// 
- function ChasePlayer() 
- {
-     transform.LookAt(Player);
-     
-     if(Vector3.Distance(transform.position,Player.position) >= MinDist){
-     
-          transform.position += transform.forward*MoveSpeed*Time.deltaTime;
- 
-           
-           
-          if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
-              {
-                 //Here Call any function U want Like Shoot at here or something
-    } 
-    
-    }
- }
+var Speed = 1;
+//var FastSpeed = 20;
+//var MaxDist = 10;
+//var MinDist = 5;
+var height : int;
 
+function OnTriggerEnter(){
+	animator.enabled = false;
+
+}
+
+function OnTriggerStay(){
+		ChasePlayer();
+}
+
+function OnTriggerExit(){
+	animator.enabled = true;
+
+}
+
+//Following script modified from http://answers.unity3d.com/questions/760751/monster-patrolling.html
+
+function Start () 
+{
+
+}
+
+function ChasePlayer() 
+{
+ transform.LookAt(Vector3(Player.position.x,height,Player.position.z));
+
+ var Enemy = GameObject.FindWithTag("Enemy");
+ var distance = Vector3.Distance(Enemy.transform.position, Player.transform.position);
+
+ distance = Mathf.Abs(1-distance*.01);
+
+ print(distance);
+transform.position += (transform.forward * Speed * Time.deltaTime) * distance;
+
+
+
+// if(Vector3.Distance(transform.position,Player.position) < MaxDist){
+// 	yield WaitForSeconds (0.5);
+//      transform.position += transform.forward * FastSpeed * Time.deltaTime;
+//      print("I'm far");
+//}
+//
+// if(Vector3.Distance(transform.position,Player.position) <= MinDist){
+//      transform.position += transform.forward * SlowSpeed * Time.deltaTime;
+//      print("I'm close");
+//}
+
+
+}

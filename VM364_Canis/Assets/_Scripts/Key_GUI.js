cb22299da36fa9c04c03f8fb71ed37ece590d592
pyrosousa <sean.m.cardwell@gmail.com>
2016-04-18 12:26:10 -0400

    $1

---
+++ VM364_Canis/Assets/_Scripts/Key_GUI.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var keyFound : boolean = false;
+var keyTex : GameObject;
+
+function Awake () {
+	keyTex.SetActive(false);
+}
+
+function Update() {
+	if (keyFound == true) {
+		keyTex.SetActive(true);
+	}
+}
--- VM364_Canis/Assets/_Scripts/MiniMap.js
+++ VM364_Canis/Assets/_Scripts/MiniMap.js
@@ -1,11 +1,17 @@
 ﻿#pragma strict
 
+static var miniMap : Camera;
 static var Player : GameObject;
 
 function Awake () {
 	Player = GameObject.FindWithTag("Player");
+	miniMap = GameObject.Find("MiniMapCam").GetComponent.<Camera>();
+}
+
+function Start () {
+//	miniMap.rect = new Rect(0,0,.2,.2);
 }
 
 function Update () {
-	transform.position = Vector3(Player.transform.position.x,400,Player.transform.position.z); 
+	transform.position = Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
 }

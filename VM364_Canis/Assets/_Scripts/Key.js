32e30f83e6496d6f377087abcf1075b95898563e
pyrosousa <sean.m.cardwell@gmail.com>
2016-04-18 00:09:40 -0400

    Scripting Work Mainly Keys
    Can collect keys, keys glow after finding safe house, safe house
    prepped for getting the actual assets

---
+++ VM364_Canis/Assets/_Scripts/Key.js
@@ -0,0 +1,45 @@
+#pragma strict
+
+static var myGlobals : GameObject;
+static var keyCollector : Key_Collector;
+static var distanceToKey : float = 25; // maximum distance that the raycast will detect
+
+var safeHouseStatus : boolean;
+var thisKey : GameObject;
+var halo : Behaviour;
+
+function Awake () {
+	myGlobals = GameObject.FindWithTag("System");
+	keyCollector = myGlobals.GetComponent("Key_Collector");
+	halo.enabled = false; 
+}
+
+function Update () {
+	safeHouseStatus = keyCollector.foundSafeHouse;
+	if (safeHouseStatus == true) {
+		halo.enabled = true;
+		} else {
+		halo.enabled = false;
+		}
+	Pickup();
+}
+
+//function onMouseUp () {
+//	Destroy(gameObject);
+//	keyCollector.keyPiece += 1;
+//}
+
+function Pickup() {
+if ( Input.GetMouseButtonDown(0) || Input.GetKeyDown( KeyCode.E ) ) {
+	var hit : RaycastHit; var rayPos : Ray = Camera.main.ScreenPointToRay( Vector3( Screen.width * 0.5, Screen.height * 0.5, 0 ) );
+	if ( Physics.Raycast( rayPos, hit, distanceToKey ) ){
+//			Debug.Log( "Ray hit : " + hit.collider.gameObject.name );
+//			Debug.DrawLine( Camera.main.transform.position, hit.point, Color.red, 1.5 );
+			if ( hit.collider.gameObject.name == thisKey.name  ) {
+//				Debug.Log( "Ray hit key for sure" );
+				keyCollector.keyPiece += 1;
+				Destroy( hit.collider.gameObject );
+			}
+		}
+	}
+}
---
+++ VM364_Canis/Assets/_Scripts/MiniMap.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+static var Player : GameObject;
+
+function Awake () {
+	Player = GameObject.FindWithTag("Player");
+}
+
+function Update () {
+	transform.position = Vector3(Player.transform.position.x,400,Player.transform.position.z); 
+}
